5e7630506713cbf1813f280a86ed2555e59aebd4
Zharnite <zharnite@gmail.com>
2018-05-26 13:58:42 -0400

    created js tasks for prisoners dilemma

---
+++ games/js/prisonersdilemma.js
@@ -0,0 +1,11 @@
+results = document.getElementById('resutls');
+
+//9-1 chance that friend will betray
+
+function betray() {
+	//add text and image of getting years and betraying friend
+}
+
+function dontbetray() {
+ //add text and image of getting years and not betraying
+}
